d7d6605d6c671d2bcf8bd416b1b888b918fbcd26
Philippe Thierry <phil@reseau-libre.net>
2018-10-23 15:19:06 +0200

    [enhancement] updated RNG source for kernel-based RNG

--- aes.c
+++ aes.c
@@ -3,51 +3,11 @@
 #include "api/types.h"
 #include "api/print.h"
 #include "api/syscall.h"
+#include "api/random.h"
 #include "librng.h"
 
 // FIXME: used for bench (key injection in cryp). to be updated
 uint32_t id_smart = 1;
-
-/*
- * FIXME: get_random should be hosted elsewhere in librng) not here
- */
-int get_random(unsigned char *buf, uint16_t len)
-{
-    uint16_t i;
-    uint8_t ret;
-
-    /* First, set the buffer to zero */
-    memset(buf, 0, len);
-
-    /* Generate as much random as necessary */
-    for(i = 0; i < sizeof(uint32_t) * (len / sizeof(uint32_t)); i += sizeof(uint32_t)){
-        if((ret = sys_get_random((char*)(&(buf[i])), 4))){
-            printf("error while getting random ! ret=%d\n", ret);
-            goto err;
-        }
-    }
-    if((len - i) > (int16_t)sizeof(uint32_t)){
-        /* We should not end here ... */
-        goto err;
-    }
-    /* Handle the remaining bytes */
-    if(i < len){
-        uint32_t random;
-        if((ret = sys_get_random(((char*)&random), 4))){
-            printf("error while getting random ! ret=%d\n", ret);
-            goto err;
-        }
-        while(i < len){
-            buf[i] = (random >> (8 * (len - i))) & 0xff;
-            i++;
-        }
-    }
-
-    return 0;
-err:
-    return -1;
-}
-
 
 
 #ifdef CONFIG_USR_LIB_AES_ALGO_CRYP_SUPPORT
